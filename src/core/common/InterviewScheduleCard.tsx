f9b58067a9c98e906abcc38980360277555f0e6d
syogesh565 <syogesh565@gmail.com>
2025-09-10 01:10:29 +0530

    candidate redirection in interviewcard fixed-2

--- src/core/common/InterviewScheduleCard.tsx
+++ src/core/common/InterviewScheduleCard.tsx
@@ -113,7 +113,7 @@ const InterviewScheduleCard: React.FC<InterviewScheduleCardProps> = ({ employeeI
   };
 
   const handleViewCandidate = (candidateId: string) => {
-    navigate(`${routes.candidatesGrid}?viewCandidate=${candidateId}`);
+    navigate(routes.candidatesGrid);
   };
 
   return (
